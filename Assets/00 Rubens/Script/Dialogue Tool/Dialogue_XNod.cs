80f0a0af77510e1765adf4587d457057860c5766
Rubyaad <92214790+Rubyaad@users.noreply.github.com>
2023-03-04 11:37:14 +0100

    Node Dialogue nice look

--- Assets/00 Rubens/Script/Dialogue Tool/Dialogue_XNod.cs
+++ Assets/00 Rubens/Script/Dialogue Tool/Dialogue_XNod.cs
@@ -3,25 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using XNode;
 
+[NodeWidth(400)]
+[NodeTint("#282828")]
 public class Dialogue_XNod : Node {
 
 	public int Tag;
 
-	[TextArea] 
+	[TextArea(3, 10)] 
 	public string Dialogue;
 
 	[Range(1, 9)] 
-	public int Priority;
+	public int Priority = 1;
 
-	public AudioClip SonDialogue;
+	public AudioClip AudioClipDialogue;
 
-	[TextArea] 
-	public string Context;
+	[TextArea(4,10)] 
+	public string Annotation;
 
 	public float BufferTime = 1f;
 
 	public bool HasBeenPlayed = false;
 
+	public bool IsRecorded;
+
+	public bool IsIntegrated;
+
+	[Input] public Dialogue_XNod PreviousDialogue;
+
+	[Output] public Dialogue_XNod NextDialogue;
 
 
 
@@ -35,8 +44,13 @@ public class Dialogue_XNod : Node {
 		}
 	}
 
+	public override object GetValue(NodePort port)
+	{
+		return this;
+	}
 
-    private void OnDestroy()
+
+		private void OnDestroy()
     {
 		(graph as DialogueToolGraph_XNod).OnDestructionOfDialogueNode(Tag);
 	}
--- Assets/00 Rubens/Script/Dialogue Tool/Editor/Editor_Dialogue_XNod.cs
+++ Assets/00 Rubens/Script/Dialogue Tool/Editor/Editor_Dialogue_XNod.cs
@@ -9,20 +9,157 @@ using XNodeEditor;
 public class Editor_Dialogue_XNod : NodeEditor
 {
 
-    Dialogue_XNod _curent_object;
+    Dialogue_XNod _curentObject;
+
+    bool _showImportantInformations = true;
+    bool _showDetails;
+    bool _showAnnotation;
+    bool _showRealTimeInformation;
+
+    Color _backgroundColor;
 
     public override void OnBodyGUI()
     {
-        if (_curent_object == null)
+        if (_curentObject == null)
         {
-            _curent_object = target as Dialogue_XNod;
+            _curentObject = target as Dialogue_XNod;
         }
 
         serializedObject.Update();
 
-        _curent_object.name = _curent_object.Tag.ToString().PadLeft(6, '0');
+        _curentObject.name = _curentObject.Tag.ToString().PadLeft(6, '0');
 
+        Color previousColor = GUI.backgroundColor;
+
+        EditorGUILayout.BeginHorizontal();
+
+        if (_curentObject.IsIntegrated)
+        {
+            GUI.backgroundColor = Color.green;
+            EditorGUILayout.LabelField("Integrated", EditorStyles.miniButtonMid);
+        }
+        else
+        {
+            GUI.backgroundColor = Color.red;
+            EditorGUILayout.LabelField("Not integrated", EditorStyles.miniButtonMid);
+        }
+
+        if (_curentObject.IsRecorded)
+        {
+            GUI.backgroundColor = Color.green;
+            EditorGUILayout.LabelField("Recorded", EditorStyles.miniButtonMid);
+        }
+        else
+        {
+            GUI.backgroundColor = Color.red;
+            EditorGUILayout.LabelField("Not recorded", EditorStyles.miniButtonMid);
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        GUI.backgroundColor = previousColor;
+
+        _showImportantInformations = EditorGUILayout.BeginFoldoutHeaderGroup(_showImportantInformations, "Show importants informations");
+
+        if (_showImportantInformations)
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("Dialogue"));
+
+            EditorGUILayout.Space(5);
+
+            //EditorGUILayout.LabelField("Priority :");   , new GUIContent("")
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("Priority"));
+
+            EditorGUILayout.Space(5);
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("AudioClipDialogue :");
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("AudioClipDialogue"), new GUIContent(""));
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.EndVertical();
+        }
+
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        EditorGUILayout.Space(5);
+
+        _showDetails = EditorGUILayout.BeginFoldoutHeaderGroup(_showDetails, "Show details");
+
+        if (_showDetails)
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("BufferTime"));
+
+            EditorGUILayout.EndVertical();
+        }
+
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        EditorGUILayout.Space(5);
+
+        _showAnnotation = EditorGUILayout.BeginFoldoutHeaderGroup(_showAnnotation, "Show annotations");
+
+        if(_showAnnotation)
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("Annotation"));
+
+            EditorGUILayout.Space(5);
+
+            EditorGUILayout.BeginHorizontal();
+
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("IsIntegrated"));
+
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("IsRecorded"));
+
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.EndVertical();
+        }
+
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        EditorGUILayout.Space(5);
+
+        _showRealTimeInformation = EditorGUILayout.BeginFoldoutHeaderGroup(_showRealTimeInformation, "Show real-time informations");
+
+        if (_showRealTimeInformation)
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+            EditorGUILayout.LabelField("Has been played : " + _curentObject.HasBeenPlayed.ToString());
+
+            EditorGUILayout.EndVertical();
+        }
+
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        EditorGUILayout.Space(15);
+
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+        EditorGUILayout.LabelField("Input", EditorStyles.whiteLargeLabel);
+
+        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("PreviousDialogue"));
+
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+        EditorGUILayout.LabelField("Output", EditorStyles.whiteLargeLabel);
+
+        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("NextDialogue"));
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndVertical();
 
     }
-
 }
