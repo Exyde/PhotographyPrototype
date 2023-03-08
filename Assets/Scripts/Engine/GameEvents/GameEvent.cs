8578d18f96da1055986d3680fd7d5dbb9cbaaf7d
Exyde <54008677+Exyde@users.noreply.github.com>
2023-03-08 19:47:26 +0100

    Trigger Events Refactoring

--- Assets/Scripts/Engine/GameEvents/GameEvent.cs
+++ Assets/Scripts/Engine/GameEvents/GameEvent.cs
@@ -43,19 +43,21 @@ namespace Core.GameEvents{
         string _contextName;
     }
 
-[System.Serializable]
-public struct FactCondition{
-    [SerializeField] string _blackboardName;
-    [Space(5)]
-    [SerializeField] string _factName;
-    [SerializeField] Comparaison _comparaison;
-    [SerializeField] int _value;
-}
+    [System.Serializable]
+    public struct FactCondition{
+        [SerializeField] string _blackboardName;
+        [Space(5)]
+        [SerializeField] string _factName;
+        [SerializeField] Comparaison _comparaison;
+        [SerializeField] int _value;
+    }
 
-public enum Comparaison{
-    Equal, Different, Superior, SuperiorOrEqual, Inferior, InferiorOrEqual
+    public enum Comparaison{
+        Equal, Different, Superior, SuperiorOrEqual, Inferior, InferiorOrEqual
     }
 }
+
+
 
 
 
--- Assets/Scripts/Engine/GameEvents/Trigger/OnTriggerGameEvent.cs
+++ Assets/Scripts/Engine/GameEvents/Trigger/OnTriggerGameEvent.cs
@@ -7,16 +7,16 @@ namespace Core.GameEvents{
     [RequireComponent(typeof(BoxCollider))]
     public class OnTriggerGameEvent : GameEvent { //@TODO : Splits in 3 Distinct classes Enter, StayTimer, Exit
 
-        public enum TriggerDetectionMode {Layer, Tag};
-        public enum TriggerMode {Enter, Stay, Exit};
+        protected enum TriggerDetectionMode {Layer, Tag};
+        protected enum TriggerMode {Enter, Stay, Exit};
 
         private Collider coll;
 
         [Header("Settings")]
-        [SerializeField] TriggerDetectionMode _triggerDetectionMode = TriggerDetectionMode.Layer;
-        [SerializeField] TriggerMode _triggerMode = TriggerMode.Enter;
-        [SerializeField] LayerMask _triggerLayer;
-        [SerializeField] string _triggerTag;
+        [SerializeField] protected TriggerDetectionMode _triggerDetectionMode = TriggerDetectionMode.Layer;
+        [SerializeField] protected TriggerMode _triggerMode = TriggerMode.Enter;
+        [SerializeField] protected LayerMask _triggerLayer;
+        [SerializeField] protected string _triggerTag;
  
         void Reset(){
             coll = GetComponent<Collider>();
@@ -26,16 +26,6 @@ namespace Core.GameEvents{
             coll.isTrigger = true;
         }
 
-        private void OnTriggerEnter(Collider other){
-            if (_triggerMode != TriggerMode.Enter) return;
-            if (_triggerDetectionMode == TriggerDetectionMode.Layer && other.gameObject.layer == _triggerLayer){
-                base.DispatchEvent();
-            }
-
-            if (_triggerDetectionMode == TriggerDetectionMode.Tag && other.gameObject.tag == _triggerTag){
-                base.DispatchEvent();
-            }
-        }
 
         private void OnTriggerStay(Collider other){
             if (_triggerMode != TriggerMode.Stay) return;
