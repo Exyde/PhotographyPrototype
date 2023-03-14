f0777e07010ded1fbea6dbf94cb5e6b2688fc746
Exyde <54008677+Exyde@users.noreply.github.com>
2023-03-14 16:28:41 +0100

    Trigger Stay and Timer Done

--- Assets/00_Younes/Scripts/Engine/GameEvents/Trigger/OnTriggerStayEvent.cs
+++ Assets/00_Younes/Scripts/Engine/GameEvents/Trigger/OnTriggerStayEvent.cs
@@ -3,18 +3,41 @@ using UnityEngine; //@TODO : Add Stay timing behavior here
 namespace Core.GameEvents{
     public class OnTriggerStayEvent : OnTriggerGameEvent{
    
+        [Space (10)]
+        [SerializeField] Timer _timer;
+
         protected override void SetEventType()
         {
             _eventName = EventName.TRIGGER_STAY; 
         }
         private void OnTriggerStay(Collider other){
+
             if (_triggerDetectionMode == TriggerDetectionMode.Layer && other.gameObject.layer == _triggerLayer){
-                base.DispatchEvent();
+                _timer.Update();
             }
 
             if (_triggerDetectionMode == TriggerDetectionMode.Tag && other.gameObject.tag == _triggerTag){
-                base.DispatchEvent();
+                _timer.Update();
             }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if (_triggerDetectionMode == TriggerDetectionMode.Layer && other.gameObject.layer == _triggerLayer){
+                _timer.ResetTimerValue();
+            }
+
+            if (_triggerDetectionMode == TriggerDetectionMode.Tag && other.gameObject.tag == _triggerTag){
+                _timer.ResetTimerValue();
+            }
+        }
+
+        private void OnEnable() {
+            _timer.OnTimerEnd += base.DispatchEvent;
+            Debug.Log("Registered event");
+        }
+
+        private void OnDisable() {
+            _timer.OnTimerEnd -= base.DispatchEvent;      
         }
     } 
 }
