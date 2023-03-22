090338cebd8805028bcfe2d77e451dd908a8ae53
Exyde <54008677+Exyde@users.noreply.github.com>
2023-03-22 13:27:24 +0100

    Skybox Lerp v1

--- Assets/00_Younes/Scripts/Managers/MeteoManager.cs
+++ Assets/00_Younes/Scripts/Managers/MeteoManager.cs
@@ -7,6 +7,7 @@ public class MeteoManager : MonoBehaviour, IGameEventManager {
 
     public Material _militarySkybox;
     public Material _terraformingSkybox;
+    public SkyboxLerper _skyboxLerper;
 
     private void OnGameEvent_MeteoManager(EventName eventName, string senderName){
 
@@ -68,5 +69,84 @@ public class MeteoManager : MonoBehaviour, IGameEventManager {
         }
     }
 
-    //Skybox Lerp
+    //Skybox Lerp - Need SkyboxConfig ?
+    [ContextMenu("Lerp Skybox A->B")]
+    public void LerpSkyboxSettingsContextMenuAB(){
+        StartCoroutine(_skyboxLerper.LerpSkyboxSettings(_skyboxLerper._skyboxSettingsA, _skyboxLerper._skyboxSettingsB));
+    }
+
+        [ContextMenu("Lerp Skybox B->A")]
+    public void LerpSkyboxSettingsContextMenuBA(){
+        StartCoroutine(_skyboxLerper.LerpSkyboxSettings(_skyboxLerper._skyboxSettingsB, _skyboxLerper._skyboxSettingsA));
+    }
+}
+
+[System.Serializable]
+public class SkyboxLerper{
+    [SerializeField][Range(.5f, 30)] float _timeToLerp;
+    [SerializeField] public SkyboxSettings _skyboxSettingsA;
+    [SerializeField] public SkyboxSettings _skyboxSettingsB;
+
+    public Material matA, matB;
+
+    public IEnumerator LerpSkyboxSettings(SkyboxSettings A, SkyboxSettings B){
+
+        float elapsedTime = 0;
+
+        Material skybox = RenderSettings.skybox;
+        skybox.SetTexture("_Cloud_Texture", B._cloudTexture);
+
+
+        while (elapsedTime < _timeToLerp){
+            
+            float t = elapsedTime / _timeToLerp;
+            //skybox.Lerp(matA, matB, t);
+            
+            //Sky
+            skybox.SetColor("_SkyColor", Color.Lerp(A._skyColor, B._skyColor, t));
+            skybox.SetColor("_HorizonColor", Color.Lerp(A._horizonColor, B._horizonColor, t));
+
+            //Stars
+            skybox.SetFloat("_Star_Density", Mathf.Lerp(A._starsDensity, B._starsDensity, t));
+
+            //Cloud
+            skybox.SetColor("_Cloud_Color", Color.Lerp(A._cloudColor, B._cloudColor, t));
+            skybox.SetFloat("_Cloud_Speed", Mathf.Lerp(A._cloudSpeed, B._cloudSpeed, t));
+            skybox.SetFloat("_Cloud_Height", Mathf.Lerp(A._cloudHeight, B._cloudHeight, t));
+            skybox.SetFloat("_Cloud_Edge", Mathf.Lerp(A._cloudEdge, B._cloudEdge, t));
+
+            //Astral
+            skybox.SetColor("_Sun_Color", Color.Lerp(A._sunColor, B._sunColor, t));
+            skybox.SetFloat("_SunSize", Mathf.Lerp(A._sunSize, B._sunSize, t));
+            skybox.SetFloat("_Sun_Mask_Size", Mathf.Lerp(A._sunMaskSize, B._sunMaskSize, t));
+            
+            elapsedTime += Time.deltaTime;
+            //RenderSettings.skybox = skybox;
+            yield return null;
+        }
+    }
+}
+
+[System.Serializable]
+public struct SkyboxSettings{
+    [Header ("Sky")]
+    [SerializeField] public Color _skyColor;
+    [SerializeField] public Color _horizonColor;
+
+    [Header ("Stars")]
+    [SerializeField][Range(0, 1000)] public float _starsDensity;
+
+    [Header ("Clouds")]
+    [SerializeField] public Texture _cloudTexture;
+    [SerializeField] public Color _cloudColor;
+    [SerializeField][Range(0, 1)] public float _cloudSpeed;
+    [SerializeField][Range(0, 1)] public float _cloudHeight;
+    [SerializeField][Range(0, 1)] public float _cloudEdge;
+
+    [Header ("Astral Plane")]
+    [ColorUsageAttribute(true,true)]
+    [SerializeField] public Color _sunColor;
+    [SerializeField][Range(0, 2)] public float _sunSize;
+    [SerializeField][Range(0, 1)] public float _sunMaskSize;
+
 }
