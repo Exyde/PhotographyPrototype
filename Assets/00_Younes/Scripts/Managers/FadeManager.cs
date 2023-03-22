9d4f023c34c034dcd3565ea5d087e6e100810396
Exyde <54008677+Exyde@users.noreply.github.com>
2023-03-22 12:09:29 +0100

    Fade Manager Update

--- Assets/00_Younes/Scripts/Managers/FadeManager.cs
+++ Assets/00_Younes/Scripts/Managers/FadeManager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +7,8 @@ public class FadeManager : MonoBehaviour
     public static FadeManager _instance;
 
     [SerializeField][Range(.1f, 3f)] float _defaultTransitionDuration = 1f;
+    [SerializeField][Range(.1f, 3f)] float _blackTimeDuration = .5f;
+
     [SerializeField] Image _screenFader;
     [SerializeField] Color _normalColor;
     [SerializeField] Color _blackColor;
@@ -21,38 +22,56 @@ public class FadeManager : MonoBehaviour
         }
     }
 
-    public IEnumerator FadeToBlack(){
+    private void OnEnable() {
+        StoryManager.EndOfDay += FullFadeCycleCallback;
+    }
+
+    private void OnDisable(){
+        StoryManager.EndOfDay -= FullFadeCycleCallback;
+    }
+
+    public IEnumerator FullFadeCycle(){
+        yield return StartCoroutine(FadeToBlack(_defaultTransitionDuration));
+        yield return new WaitForSeconds(_blackTimeDuration);
+        yield return StartCoroutine(FadeToNormal(_defaultTransitionDuration));
+    }
+
+    public void FullFadeCycleCallback(){
+        StartCoroutine(FullFadeCycle());
+    }
+
+    public IEnumerator FadeToBlack(float timeToFade){
 
         float timeElapsed = 0;
 
-        while (timeElapsed < _defaultTransitionDuration){
+        while (timeElapsed < timeToFade){
 
             timeElapsed += Time.deltaTime;
-            _screenFader.color = Color.Lerp(_normalColor, _blackColor, timeElapsed / _defaultTransitionDuration);
+            _screenFader.color = Color.Lerp(_normalColor, _blackColor, timeElapsed / timeToFade);
             yield return null;    
         }
     }
 
-    public IEnumerator FadeToNormal(){
+    public IEnumerator FadeToNormal(float timeToFade ){
 
         float timeElapsed = 0;
 
-        while (timeElapsed < _defaultTransitionDuration){
+        while (timeElapsed < timeToFade){
 
             timeElapsed += Time.deltaTime;
-            _screenFader.color = Color.Lerp(_blackColor, _normalColor, timeElapsed / _defaultTransitionDuration);
+            _screenFader.color = Color.Lerp(_blackColor, _normalColor, timeElapsed / timeToFade);
             yield return null;    
         }
     }
 
     [ContextMenu("Fade To Black")]
-    public void FadeToBlackButton(){
-        StartCoroutine(FadeToBlack());
+    public void FadeToBlackCallback(){
+        StartCoroutine(FadeToBlack(_defaultTransitionDuration));
     }
 
     [ContextMenu("Fade To Normal")]
-    public void FadeToNormalButton(){
-        StartCoroutine(FadeToNormal());
+    public void FadeToNormalCallback(){
+        StartCoroutine(FadeToNormal(_defaultTransitionDuration));
     }
 
 }
