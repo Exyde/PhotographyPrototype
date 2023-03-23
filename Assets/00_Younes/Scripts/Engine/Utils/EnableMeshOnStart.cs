8c3b3d3e6fa83724cbb05153ed567f8f3b67b894
Exyde <54008677+Exyde@users.noreply.github.com>
2023-03-23 21:31:36 +0100

    Ocean Optimization v1

---
+++ Assets/00_Younes/Scripts/Engine/Utils/EnableMeshOnStart.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[RequireComponent (typeof(MeshRenderer))]
+public class EnableMeshOnStart : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = true;
+    }
+}
